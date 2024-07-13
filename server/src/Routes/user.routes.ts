8c7efdd233574f358a91d4e60b33a34b6c3eda21
ft.souptik <mr.souptiktaran@gmail.com>
2024-07-13 17:48:56 +0530

    v0.4.0 : Google Auth Added

--- server/src/Routes/user.routes.ts
+++ server/src/Routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import * as userController from '../controller/user.controller';
 import { authenticateToken } from "../middlewares/auth";
+import {passport} from '../services/passportSetup' ;
 const router = Router()
 
 router.post('/Signup' , userController.signup);
@@ -8,6 +9,8 @@ router.post('/login' , userController.login);
 router.get('/test' , authenticateToken , (req: Request , res : Response)=>{
     res.send("Done  ")
 })
+router.get('/login/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+router.get('/google/redirect',passport.authenticate('google', { session: false , failureRedirect : 'http://localhost:5173/error' }) ,  userController.googleRedirect)
 
 
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,15 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import routes from './Routes/index'
 import cors from 'cors'
-import { Welcomeworker } from "./services/worker";
-Welcomeworker ;
+import {passport} from './services/passportSetup'
+// import { Welcomeworker } from "./services/worker";
+// Welcomeworker ;
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+app.use(passport.initialize())
 
 app.use(cors({
   origin: 'http://localhost:5173', 
